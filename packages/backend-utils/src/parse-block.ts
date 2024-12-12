35481e36a8bae5729670eb593fb7a8b268eece0f
Cryptoryda <113293883+cryptoryda@users.noreply.github.com>
2024-12-12 15:37:06 +0000

    parse-block fiel update with parsing method

--- packages/backend-utils/src/parse-block.ts
+++ packages/backend-utils/src/parse-block.ts
@@ -2,30 +2,38 @@ import { L2Block } from "@aztec/aztec.js";
 import { ChicmozL2Block, chicmozL2BlockSchema } from "@chicmoz-pkg/types";
 
 const getTxEffectWithHashes = (txEffects: L2Block["body"]["txEffects"]) => {
-  return txEffects.map((txEffect) => {
-    return {
-      ...txEffect,
-      hash: "0x" + txEffect.hash().toString("hex"),
-    };
-  });
+  return txEffects.map((txEffect) => ({
+    ...txEffect,
+    hash: "0x" + txEffect.hash().toString("hex"),
+  }));
 };
 
 export const blockFromString = (stringifiedBlock: string): L2Block => {
-  return L2Block.fromString(stringifiedBlock);
-}
+  try {
+    return L2Block.fromString(stringifiedBlock);
+  } catch (error) {
+    throw new Error("Failed to parse L2Block from string");
+  }
+};
 
-export const parseBlock = (b: L2Block): ChicmozL2Block => {
-  const blockHash = b.hash();
-  const blockWithTxEffectsHashesAdded = {
-    ...b,
-    body: {
-      ...b.body,
-      txEffects: getTxEffectWithHashes(b.body.txEffects),
-    },
-  };
-  return chicmozL2BlockSchema.parse({
-    hash: blockHash.toString(),
-    height: b.number,
-    ...JSON.parse(JSON.stringify(blockWithTxEffectsHashesAdded)),
-  });
+export const parseBlock = (block: L2Block): ChicmozL2Block => {
+  try {
+    const blockHash = block.hash();
+    const blockWithTxEffectsHashesAdded = {
+      body: {
+        ...block.body,
+        txEffects: getTxEffectWithHashes(block.body.txEffects),
+      },
+    };
+
+    const parsedBlock = {
+      hash: blockHash.toString(),
+      height: block.number,
+      ...blockWithTxEffectsHashesAdded,
+    };
+
+    return chicmozL2BlockSchema.parse(parsedBlock);
+  } catch (error) {
+    throw new Error("Failed to parse ChicmozL2Block");
+  }
 };
